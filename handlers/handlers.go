afb951aa920ef3c13b6188c3a400f6ab7762f58c
Sam Inman <sinman@caci.co.uk>
2021-08-13 16:40:01 +0100

    Updated worldcup web service

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -38,10 +38,13 @@ func AddNewWinner(res http.ResponseWriter, req *http.Request) {
 	if !isTokenValid {
 		res.WriteHeader(http.StatusUnauthorized)
 	} else {
-		data.AddNewWinner(req.Body)
+		err := data.AddNewWinner(req.Body)
+		if err != nil {
+			res.WriteHeader(http.StatusUnprocessableEntity)
+			return
+		}
 		res.WriteHeader(http.StatusCreated)
 	}
-
 }
 
 // WinnersHandler is the dispatcher for all /winners URL
